561d0e6c6aa99a56f5bc95952c061c354d97958c
tes <scalavision@nokiamail.com>
2016-05-01 18:16:40 +0200

    getting somewhere with scala meta :-)

--- src/main/scala-2.11/record/generator/DomainModel.scala
+++ src/main/scala-2.11/record/generator/DomainModel.scala
@@ -2,13 +2,25 @@ package record.generator
 
 import java.util.Date
 
+import record.generator.TqlTest.{CleanModel, MongoTemplateData}
+
+import scala.meta.Term
+
 trait MongoRecordMetaGen {
-  case class MRec[ClassName, MongoPrimaryKeyType]()
+
+  case class MRec[ClassName, MongoPrimaryKeyType](
+    optionalFields: ClassName => List[Any] = (a:ClassName) => Nil,
+    lengthFields: ClassName => List[(String,Int)] = (a:ClassName) => Nil
+  )
+
+  case class BRec[ClassName]()
+
   type StringPk
   type ObjectIdPk
   type UUIDPk
   type IntPk
   type LongPk
+
 }
 
 object DomainModel {
@@ -22,11 +34,85 @@ object DomainModel {
 
   case class Fnr(fnr:String)
 
+  case class Person(
+    name:String
+  )
+
 }
 
 object MongoRecordMeta extends MongoRecordMetaGen {
   import DomainModel._
 
-  MRec[Applicant,UUIDPk]
+  MRec[Applicant,UUIDPk](
+    optionalFields =
+      (a:Applicant) => List(
+        a.firstName,
+        a.lastName,
+        a.born,
+        a.fnr
+      ),
+    lengthFields =
+      (a:Applicant) => List(
+        (a.firstName, 200),
+        (a.lastName, 300)
+      )
+  )
+
+  BRec[Fnr]
+  MRec[Person, ObjectIdPk]()
+
+}
+
+object MongoMetaHelpers {
+
+  val optionalField = Term.Name("optionalFields")
+  val lengthField = Term.Name("lengthFields")
+  case class FieldLength(name:String, length:Int)
+
+  sealed trait SpecialField
+  case class OptionLengthField(name:String, length:Int) extends SpecialField
+  case class OptionField(name:String) extends SpecialField
+  case class LengthField(length:Int) extends SpecialField
+
+}
+
+object MongoRecordTemplate {
+  import MongoMetaHelpers._
+
+  import scala.meta.Term
+
+  private def createParam(param:Term.Param, specialFields:Map[String,SpecialField]):String = {
+
+    specialFields.get(param.name.toString()) match {
+      case Some(specialField) =>
+        specialField match {
+          case OptionField(name) =>
+            s"""
+               |  object ${param.name} extends ${param.decltpe.get}(this, ${param.decltpe.get}) {
+               |    override def optional_? = true
+               |  }""".stripMargin
+          case LengthField(length) =>
+            s"""object ${param.name} extends ${param.decltpe.get}(this, $length)"""
+
+          case OptionLengthField(name, length) =>
+            s"""
+               |  object ${param.name} extends ${param.decltpe.get}(this, $length) {
+               |    override def optional_? = true
+               |  }""".stripMargin
+        }
+      case None =>
+        s"""object ${param.name} extends ${param.decltpe.get}(this, ${param.decltpe.get})"""
+    }
+
+  }
+
+  def createSource(metaData:MongoTemplateData, cleanModel:CleanModel):String  =
+    s"""
+       |class ${cleanModel.name.value} private() extends MongoRecord[${cleanModel.name.value}] with ${metaData.pkType}[${cleanModel.name.value}] {
+       | def meta = ${cleanModel.name.value}
+       | ${cleanModel.params.map(p => createParam(p, metaData.specialFields)).mkString("\n ")}
+       |}
+       |object ${cleanModel.name.value} extends ${cleanModel.name.value} with MongoMetaRecord[${cleanModel.name.value}]
+     """.stripMargin
 
 }
--- src/main/scala-2.11/record/generator/TqlTest.scala
+++ src/main/scala-2.11/record/generator/TqlTest.scala
@@ -1,8 +1,9 @@
 package record.generator
 
+import java.io.FileInputStream
 import scala.meta._
 import scala.meta.dialects.Scala211
-import java.io.FileInputStream
+import MongoMetaHelpers._
 
 object TqlTest {
 
@@ -13,48 +14,6 @@ object TqlTest {
 
   val stream = new FileInputStream(sourcePath)
   val tree: Parsed[Source] = stream.parse[Source]
-
-  case class CleanModel(
-    name: Type.Name,
-    params: Seq[Seq[Term.Param]]
-  )
-
-  case class MongoMapper(
-    typeParameters: Seq[Type]
-  )
-
-  case class MongoModel(
-    primaryId:Type,
-    name: Type.Name,
-    params:Seq[Seq[Term.Param]]
-  )
-
-  def disassembleDomainModel(
-    tree: scala.meta.Template
-  ): Seq[Any] = tree.children.collect {
-    case q"..$mods class $tname (...$paramss)" =>
-      CleanModel(tname, paramss)
-  }
-
-  def disassembleMongoMapping(
-    tree: scala.meta.Template
-  ):Seq[Any] = tree.children.collect {
-    case q"$tpe [..$tpes] " =>
-      MongoMapper(tpes)
-  }
-
-  def parseTree(
-    tree: Parsed[Source],
-    targetModel:String,
-    disassembler: Template => Seq[Any]
-  ): List[Any] = tree.get.collect {
-    case q"..$mods object $name extends $template" =>
-      name match {
-        case Term.Name(objectName) =>
-          if(objectName == targetModel)
-            disassembler(template)
-      }
-  }
 
   def test(): Unit = {
 
@@ -70,15 +29,159 @@ object TqlTest {
       disassembleMongoMapping
     )(1).asInstanceOf[List[MongoMapper]]
 
-    val records = for {
-      model <- cleanModel
-      mapper <- mongoMetaModel
+    val mongoMetaData:Map[String, MongoTemplateData] = mongoMetaModel.map{
+      m =>
+        val templateData = m.toMongoTemplateData
+        templateData.pkType -> templateData
+    }.toMap
+
+    val recordSources = for {
+      cm <- cleanModel
+      metaData <- mongoMetaData.get(cm.name.toString)
     } yield {
-      s"${model.name} ${mapper.typeParameters(1) } "
+      MongoRecordTemplate.createSource(metaData, cm)
     }
 
-    println(records)
+    println(recordSources.mkString(""))
 
   }
+
+  def disassembleDomainModel(
+    tree: scala.meta.Template
+  ): Seq[Any] = tree.children.collect {
+    case q"..$mods class $tname (...$paramss)" =>
+      CleanModel(tname, paramss(0))
+  }
+
+  def disassembleOptionalFields(tree: Seq[Term.Arg]) : Seq[String] =
+    tree.flatMap {
+      case Term.Arg.Named(Term.Name("optionalFields"), Term.Function(param)) =>
+        param._2.collect {
+          case Term.Select(Term.Name(ref), Term.Name(fieldName)) =>
+            fieldName
+        }
+      case _ => Seq.empty
+    }
+
+
+  def disassembleLengthFields(tree: Seq[Term.Arg]) : Seq[FieldLength] = {
+
+    val result:List[Seq[Any]] = tree.flatMap {
+      case Term.Arg.Named(Term.Name("lengthFields"), Term.Function(param)) =>
+        param._2.collect {
+          case Term.Select(Term.Name(ref), Term.Name(fieldName)) =>
+            fieldName
+          case Lit(length) =>
+            length
+        }
+      case _ => Seq()
+    }.sliding(2,2).toList
+
+    result.map {
+      s =>
+        FieldLength(
+          s(0).asInstanceOf[String],
+          s(1).asInstanceOf[Int]
+        )
+    }
+
+  }
+
+  def disassembleMongoMapping(
+    tree: scala.meta.Template
+  ): Seq[MongoMapper] = tree.children.collect {
+
+    case q"$tpe [..$tpes] (..$aexpr)" =>
+
+      val optionalFields:Seq[String] =
+        disassembleOptionalFields(
+          aexpr
+        )
+
+      val lengthFields:Seq[FieldLength] =
+        disassembleLengthFields(
+          aexpr
+        )
+
+      MongoMapper(tpes, optionalFields, lengthFields)
+  }
+
+  def parseTree(
+    tree: Parsed[Source],
+    targetModel: String,
+    disassembler: Template => Seq[Any]
+  ): List[Any] = tree.get.collect {
+    case q"..$mods object $name extends $template" =>
+      name match {
+        case Term.Name(objectName) =>
+          if (objectName == targetModel)
+            disassembler(template)
+      }
+  }
+
+  case class CleanModel(
+    name: Type.Name,
+    params: Seq[Term.Param]
+  )
+
+  case class MongoMapper(
+    typeParameters: Seq[Type],
+    optionalParameters: Seq[String],
+    lengthFields: Seq[FieldLength]
+  ) {
+    def toMongoTemplateData:MongoTemplateData = {
+
+      val optionalFieldsMap:Map[String, OptionField] = optionalParameters.map {
+        s =>
+          s -> OptionField(s)
+      }.toMap
+
+      val lengthFieldsMap:Map[String, LengthField] = lengthFields.map {
+        fl =>
+          fl.name -> LengthField(fl.length)
+      }.toMap
+
+      val cleanOptionals: Map[String, OptionField] =
+        for {
+          (k,v) <- optionalFieldsMap
+          if !lengthFieldsMap.contains(k)
+        } yield {
+          k -> v
+        }
+
+      val cleanLengths: Map[String, LengthField] =
+        for {
+          (k,v) <- lengthFieldsMap
+          if !optionalFieldsMap.contains(k)
+        } yield {
+          k -> v
+        }
+
+      val cleanCombos: Map[String, OptionLengthField] =
+        for {
+          (k,v) <- lengthFieldsMap
+          if optionalFieldsMap.contains(k)
+        } yield {
+          k -> OptionLengthField(
+            name = optionalFieldsMap.get(k).get.name,
+            length = v.length
+          )
+        }
+
+      val result : Map[String, SpecialField] =
+        cleanOptionals ++ cleanLengths ++ cleanCombos
+
+      MongoTemplateData(
+        typeParameters(0).toString,
+        result
+      )
+
+    }
+  }
+
+  case class MongoTemplateData(
+    pkType: String,
+    specialFields: Map[String, SpecialField]
+  )
 
 }
